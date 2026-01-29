a8e59ee6ce150269888f4983b6c08ae3c62f21bb
Petko Drianovski <opidrianovski@gmail.com>
2026-01-29 20:44:35 +0200

    Test

---
+++ backend/main.py
@@ -0,0 +1,57 @@
+#magic to run the api and shit start
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], 
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+#magic to run the api and shit end
+
+class MakeWord:
+    def __init__(self,wordde,wordbg,word_type,**extra_info):
+        self.wordde = wordde
+        self.wordbg = wordbg
+        self.word_type = word_type
+        self.extra_info = extra_info
+    def to_dict(self):
+        data = {
+            "wordde": self.wordde,
+            "wordbg": self.wordbg,
+            "word_type": self.word_type,
+            "extra_info": self.extra_info
+        }
+        return data
+
+words = [
+    MakeWord("Hund","Куче","noun",gender = "der"),
+    MakeWord("Frau","Жена","noun",gender = "die"),
+    MakeWord("haben","Имам","verb",special_conj = False),
+    MakeWord("Sendung","pratka","noun",gender = "die")
+]
+
+
+@app.get("/")
+def hello():
+    return {"message": "Welcome to the german learning app Pich. Still in development but hey you can test it right?"}
+
+@app.get("/word")
+def get_words():
+    databox = []
+    for i in words:
+        databox.append(i.to_dict())
+    return databox
+
+@app.get("/word/{target}")
+def get_search(target:str):
+    databox = []
+    for i in words:
+        if target.lower() == i.wordde.lower() or target.lower() == i.wordbg.lower(): #for the word itself
+            return i.to_dict()
+        elif i.word_type.lower() == target.lower(): #for the type
+            databox.append(i.to_dict())
+    return databox
---
+++ frontend/script.js
@@ -0,0 +1,73 @@
+async function handleUrlNavigation() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const wordQuery = urlParams.get('word');
+    const apiUrl = "http://127.0.0.1:8000/word" + (wordQuery ? "/" + wordQuery : "");
+
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+
+        if (data.error) {
+            showError(data.error);
+        } else if (Array.isArray(data)) {
+            showList(data);
+        } else if (Array.isArray(data) == false) {
+            showCard(data);
+        }
+    } catch (err) {
+        showError("Server is offline!");
+    }
+}
+
+function showList(words) {
+    if (words[0] != undefined) {
+        const list = document.getElementById('word-list');
+        const template = document.getElementById('list-item-template');
+
+        document.getElementById('view-title').innerText = "All Words";
+        list.classList.remove('hidden');
+
+        words.forEach(word => {
+            const clone = template.content.cloneNode(true);
+            clone.querySelector('.de-text').innerText = word.wordde;
+            clone.querySelector('.bg-text').innerText = word.wordbg;
+            list.appendChild(clone);
+        });
+    }else{
+        showError("Incorrect search! Maybe check spelling...")
+    }
+}
+
+function showCard(word) {
+    document.getElementById('view-title').innerText = "Word Details";
+    document.getElementById('card-container').classList.remove('hidden');
+
+    // Pulnim htmla s JSONa ot fastapi
+    document.getElementById('card-de').innerText = word.wordde;
+    document.getElementById('card-bg').innerText = word.wordbg;
+    document.getElementById('card-type').innerText = word.word_type;
+
+    // praim nekvi vunshni raboti za da raboti
+    const Cont = document.getElementById('card-extra');
+    const row = document.getElementById('extra-row');
+    // dobavqme bezkraini extra_info
+    if (word.extra_info) {
+        // razkrivame objecta extra_info shtot se prashta ot fastapi kat obj/dict
+        Object.entries(word.extra_info).forEach(([key, value]) => {
+            const line = document.createElement('p');
+            line.innerHTML = `${key}: ${value}`;
+            Cont.appendChild(line);
+        });
+        row.classList.remove('hidden');
+    } else {
+        row.classList.add('hidden');
+    }
+}
+
+function showError(msg) {
+    const err = document.getElementById('error-msg');
+    err.innerText = msg;
+    err.classList.remove('hidden');
+}
+
+window.onload = handleUrlNavigation;
